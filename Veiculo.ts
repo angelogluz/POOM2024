792857f90950e18c9be4b930fcc618f3b17b8c44
Angelo Luz <angelogl@gmail.com>
2024-08-28 14:56:44 -0300

    Aula 1

---
+++ Veiculo.ts
@@ -0,0 +1,8 @@
+export class Veiculo{
+    marca: string = 'Padrão';
+    modelo: string = 'Padrão';
+    potencia: number = 0;
+    numeroMarchas: number = 5;
+    marchaAtual: number = 0;
+    velocidade: number = 0;
+}
---
+++ index.ts
@@ -0,0 +1,49 @@
+import { Veiculo } from "./Veiculo";
+import prompt from "prompt-sync";
+
+const teclado = prompt();
+
+console.log('Criação de veículo');
+const carro: Veiculo = criaVeiculo();
+
+while(true){
+    console.log("########### MENU ###########");
+    console.log("1 - Acelerar");
+    console.log("2 - Frear");
+    console.log("3 - Subir marcha");
+    console.log("4 - Descer marcha");
+    console.log("5 - Imprimir dados do veículo");
+    console.log("0 - Sair");
+
+    const opcao = +teclado('Escolha uma opção: ');
+    if(opcao === 0){
+        break;
+    }
+    switch (opcao) {
+        case 1:
+            acelerar(carro);
+            break;
+    
+        default:
+            break;
+    }
+}
+
+console.table(carro);
+
+function acelerar(veiculo: Veiculo): void{
+    if(veiculo.marchaAtual != 0){
+    veiculo.velocidade += veiculo.potencia*0.1;
+    console.log(veiculo.velocidade);
+}}
+
+function criaVeiculo(): Veiculo{
+    const veiculo: Veiculo = new Veiculo();
+    veiculo.marca = teclado('Marca: ');
+    veiculo.modelo = teclado('Modelo: ');
+    veiculo.potencia = +teclado('Potência: ');
+    veiculo.numeroMarchas = +teclado('Número de marchas: ');
+    return veiculo;
+}
+
+
